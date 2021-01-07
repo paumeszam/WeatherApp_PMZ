5cae69571607338e2bb5695a590231becb3c14d6
Paulina Mestre <paumeszam@gmail.com>
2021-01-07 14:45:06 -0600

    Uploading Project

---
+++ src/script.js
@@ -0,0 +1,73 @@
+//Current Date
+
+let date = new Date();
+let day = date.getDay();
+let hour = date.getHours();
+let minutes = date.getMinutes();
+
+let dayNames = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+
+function addZero(){
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return minutes;
+}
+
+let today = document.querySelector("h3");
+addZero();
+today.innerHTML = (`${dayNames[day]}, ${hour}:${minutes}`);
+
+//Input City Search
+
+function changeCity(event){
+  event.preventDefault();
+  let cityInput = document.querySelector("#search");
+  let apiKey = "3298e39870d933f506bba233ba3572ef";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&&units=metric`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+let submitButton = document.querySelector("#submit");
+submitButton.addEventListener("click",changeCity);
+
+//Current Temperature
+function showWeather(response){
+    console.log(response);
+    console.log(response.data.main.temp);
+    let h1 = document.querySelector("#degrees");
+    let temperature = Math.round(response.data.main.temp);
+    h1.innerHTML = `${temperature}°C`;
+    let h2 = document.querySelector("h2");
+    let location = (response.data.name);
+    h2.innerHTML = location;
+}
+
+//Current Location
+
+function getLocation(){
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+function showPosition(position){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    let apiKey = "3298e39870d933f506bba233ba3572ef";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`
+    axios.get(apiUrl).then(showWeather);   
+}
+
+let locationButton = document.querySelector("#location");
+locationButton.addEventListener("click", getLocation);
+
+
+
